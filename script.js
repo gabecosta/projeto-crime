77c8003c5ccb10078ea10d841c6695da64e53870
gabecosta <127759934+gabecosta@users.noreply.github.com>
2024-06-19 13:11:01 -0300

    fix tuudo

---
+++ script.js
@@ -0,0 +1,81 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Quais os métodos utilizados na sua cidade para combater o crime?",
+        alternativas: [
+            "Câmera de segurança",
+            "Sensor e de segurança"
+        ]
+    },
+    {
+        enunciado: "Quais são os fatores sociais que contribuem para o ocorrência de crimes, como roubo, violência e corrupção?  ",
+        alternativas: [
+            "Fome",
+            "Desemprego"const caixaPrincipal = document.querySelector("const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+  {
+        enunciado: "Quais os metodos utilizados na sua cidade para combater o crime",
+        alternativas: [
+…        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
+.caixa-principal");
+            const caixaPerguntas = document.querySelector(".caixa-perguntas");
+            const caixaAlternativas = document.querySelector(".caixa-alternativas");
+            const caixaResultado = document.querySelector(".caixa-resultado");
+            const textoResultado = document.querySelector(".texto-resultado");
+            
+            const perguntas = [
+              {
+                    enunciado: "Quais os metodos utilizados na sua cidade para combater o crime",
+                    alternativas: [
+            …        const botaoAlternativas = document.createElement("button");
+                    botaoAlternativas.textContent = alternativa;
+                    caixaAlternativas.appendChild(botaoAlternativas);
+                }
+            }
+            
+            mostraPergunta();
+            
+        ]
+    },
+    {
+        enunciado: "Quais ão os crimes mai ocorrentes na sua cidade?",
+        alternativas: [
+            "Tráfico de drogas",
+            "Roubo"
+        ]
+    },
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas() {
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
